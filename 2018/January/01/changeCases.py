bec30a7e830c1abf3ec4d4d446201fd73d7e7ee2
Mark Puchala II <markpuchalaii@gmail.com>
2018-01-01 13:09:45 -0600

    Update changeCases.py
    This was a very short & simple one
    I love python's methods.

--- 2018/January/01/changeCases.py
+++ 2018/January/01/changeCases.py
@@ -1,2 +1,16 @@
-# Finally working on strings!
-# More to come, once I solve this challenge
+a = "heLlo"
+print(a.swapcase())
+
+# I simply discovered Python's swapcase() function
+# It's as simple as str.swapcase()
+# WOW! Heh, that's learning for ya, right?
+
+
+#And here's what it looked like in HackerRank's challenge:
+def split_and_join(line):
+    # write your code here
+    
+if __name__ == '__main__':
+line = input()
+result = split_and_join(line)
+print(result)
