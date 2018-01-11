f296c9dcd774a4573ff1712bc60a57125af8c693
Mark Puchala II <markpuchalaii@gmail.com>
2018-01-11 00:54:42 -0600

    using lists & sets together (sort of)!
    This was a painful one.
    Unfortunately, I ended up looking up other solutions before I understood what was wrong with mine own.
    I don't. know what it would have taken to figure this out had I understood my errors before looking it up.
    Still learned, though!

---
+++ 2018/January/10/noIdea!.py
@@ -0,0 +1,26 @@
+# This challenge's lesson was reading instructions
+# I didn't read that n might have repeating values
+# So I kept getting errors when I tried to compute n as a set
+# running n as a list solved this.
+# However I could no longer run n.intersection(a)-n.intersection(b)
+# I used a solution I got from the discussion section.
+# It used a summation to quickly find (n in a) - (n in b)
+
+    # My spaghetti code version
+N,M = input().split()
+n = list(map(int, input().split()))
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))     # Pretty clever how the summation is made
+r = sum((i in a) - (i in b) for i in n)# using true == 1 & false == 0
+print(r)
+
+################################################################################
+
+    # My more refined version
+N,M = input().split()
+n,a,b = list(map(int, input().split())) , set(map(int, input().split())) , set(map(int, input().split()))
+print(sum((i in a) - (i in b) for i in n))
+
+################################################################################
+
+    # Hackerrank's approach
