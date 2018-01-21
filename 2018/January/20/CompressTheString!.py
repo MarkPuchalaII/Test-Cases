1606c67ad88776e412ac9a6dc5b84800d296c8e1
Mark Puchala II <markpuchalaii@gmail.com>
2018-01-21 00:07:09 -0600

    I learn faster with others peoples' code

--- 2018/January/20/CompressTheString!.py
+++ 2018/January/20/CompressTheString!.py
@@ -1,14 +1,15 @@
 # This challenge's lesson: Sometimes *[_ for _ in range] is better than [_ for _ in range]
+# Though maybe it's not. Still, I like this "a, b, in input()". I'm gonna test that out more.
 
     # My spaghetti code version
 from itertools import groupby
-print(*[(len(list(c)), int(k)) for k, c in groupby(input())])
+print(*[(len(list(b)), int(a)) for a, b in groupby(input())])
 
 ################################################################################
 
     # My refined version
 from itertools import groupby
-print(*[(len(list(c)), int(k)) for k, c in groupby(input())])
+[print((len(list(b)), int(a)), end = " ") for a, b in groupby(input())]
 
 ################################################################################
 
